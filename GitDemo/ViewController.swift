fd4c9026f4c0fb269a5c0d1ac08d43d0a82d5581
Konstantin <simonchik2210@gmail.com>
2020-06-18 12:51:00 +0300

    initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Kostya on 6/18/20.
+//  Copyright © 2020 Kostya Simonchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
